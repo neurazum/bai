0410a51f9a1ff6b74ec990e152ad3fc430a544bd
bai <150702266+BrAIn-AI-Academy@users.noreply.github.com>
2024-04-28 16:16:47 +0300

    Update bai-1-32c.py

--- bai-1-32c/Model Monitoring/bai-1-32c.py
+++ bai-1-32c/Model Monitoring/bai-1-32c.py
@@ -3,29 +3,21 @@ import numpy as np
 from keras.models import load_model
 import pandas as pd
 
-# Eğitilen model
-model = load_model('../../../Models/bai-1-32c.h5')
+# Modeli yükle/ Load model
+model = load_model('../Model/bai-1-32c.h5')
 
 
-# Gerçek zamanlı olarak EEG verilerini almak için fonksiyon
+# EEG verilerini okuma fonksiyonu / EEG data reading function
 def get_real_time_eeg_data():
-    # EEG verilerini CSV dosyasından okuma
-    eeg_data = pd.read_csv("../../Pre-Training/Datasets/DEAP/features_raw.csv")
-    # Veriyi uygun formata dönüştürme, gerekirse işleme
-    # Örneğin, veriyi Numpy dizisine dönüştürebilirsiniz
+    eeg_data = pd.read_csv("../Dataset/DEAP/featurew_raw.csv")
     eeg_data_np = eeg_data.to_numpy()
     return eeg_data_np
 
-print()
-
-# EEG verilerini alarak tahmin yapın ve grafik oluşturun
+# Grafik oluşturma / Create graph
 while True:
     eeg_data = get_real_time_eeg_data()
     if eeg_data is not None:
-        # EEG verilerini modelinizle tahmin edin
         prediction = model.predict(eeg_data)
-
-        # Tahmin sonuçlarını bir grafik üzerinde gösterin
         plt.figure()
         plt.plot(np.arange(len(prediction)), prediction)
         plt.xlabel('Zaman')
